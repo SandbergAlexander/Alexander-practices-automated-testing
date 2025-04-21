ceeb6ab0a0c614db761fbdfc66c8c7a4a688839c
Alexander Sandberg <Alexandersandbergnystart@gmail.com>
2025-04-21 15:19:15 +0200

    Selenium är redo att andvändas

--- C#-Test-for-simpel-home-page/Program.cs
+++ C#-Test-for-simpel-home-page/Program.cs
@@ -1,7 +1,13 @@
-﻿class Program
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+      IWebDriver driver = new ChromeDriver();
+      driver.Url =("https://www.selenium.dev/documentation/webdriver/getting_started/first_script/");
+      driver.Close();
+
     }
 }
